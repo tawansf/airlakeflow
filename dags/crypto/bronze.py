af34dc9e572e4d47bbc47753baf4236835a108e0
Tawan Silva <tawan.sf.99@gmail.com>
2026-02-14 18:23:37 -0300

    Enhance Bitcoin data ingestion and validation
    - Updated bronze.py to use environment variables for API URL and improved data handling with PostgreSQL integration.
    - Modified docker-compose.yaml to include environment file and expose PostgreSQL port.
    - Added soda checks for Bitcoin data integrity in bitcoin_checks.yaml.
    - Updated requirements.txt to include necessary dependencies.

--- dags/crypto/bronze.py
+++ dags/crypto/bronze.py
@@ -1,29 +1,37 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime, timedelta
 import requests
 import json
 import os
+from dotenv import load_dotenv
 
-BRONZE_PATH = "/opt/airflow/data/bronze/crypto"
+load_dotenv()
+
+GEEKO_URL_API = os.getenv("GEEKO_URL_API")
+BRONZE_PATH = f'{os.getenv("BRONZE_PATH")}/crypto'
 
 def bronze_ingestao_dados_bitcoin():
-    url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_last_updated_at=true"
+    url = GEEKO_URL_API
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
-
         data['processed_at'] = datetime.now().isoformat()
 
         filename = f"bitcoin_{datetime.now().strftime('%Y%m%d%H%M%S')}.json"
         filepath = os.path.join(BRONZE_PATH, filename)
-        os.makedirs(BRONZE_PATH, exist_ok=True)
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
         with open(filepath, 'w') as f:
             json.dump(data, f)
         
-        print(f"Dados do Bitcoin salvos em: {filepath}")
+        pg_hook = PostgresHook(postgres_conn_id='postgres_datawarehouse')
+        insert_sql = """INSERT INTO bronze.bitcoin_raw (payload) VALUES (%s);"""
+        pg_hook.run(insert_sql, parameters=[json.dumps(data)])
+
+        print(f'Sucesso: Arquivo {filepath} criado e dados inseridos no banco de dados.')
     else:
         raise Exception(f"Erro ao obter dados do Bitcoin: {response.status_code}")
 
